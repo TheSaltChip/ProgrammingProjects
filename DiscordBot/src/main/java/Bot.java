1aa717b5e857ae99e156d0bc9a0ec0fbf18a5bd2
TheSaltChip <seb2500@gmail.com>
2020-10-12 02:49:40 +0200

    Fixed path to token file

--- DiscordBot/src/main/java/Bot.java
+++ DiscordBot/src/main/java/Bot.java
@@ -5,16 +5,12 @@ import net.dv8tion.jda.api.JDABuilder;
 
 import javax.security.auth.login.LoginException;
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
 import java.util.Scanner;
 
 public class Bot {
     public static void main(String[] args) throws IOException {
-        String s = new File("").getAbsolutePath();
-        System.out.println(s);
-        String token = new Scanner(new File("\\src\\bot_tokens\\DiscordBot\\token.txt")).nextLine();
+        String token = new Scanner(new File("src\\BotTokens\\DiscordBot\\token.txt")).nextLine();
         JDABuilder jdaBuilder = JDABuilder.createDefault(token);
         JDA jda;
 
