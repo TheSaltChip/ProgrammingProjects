cbd4be8ddb7cd762e00599b68300f7c41598a56a
TheSaltChip <seb2500@gmail.com>
2020-10-12 02:38:41 +0200

    My First Discord Bot So far it counts the number of messages sent by a given user in the text channel where the command was written and writes n-amount of messages with a writeMessages command

--- DiscordBot/src/main/java/Bot.java
+++ DiscordBot/src/main/java/Bot.java
@@ -4,10 +4,18 @@ import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 
 import javax.security.auth.login.LoginException;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
 
 public class Bot {
-    public static void main(String[] args) {
-        JDABuilder jdaBuilder = JDABuilder.createDefault("NzY0OTU5MDk1NDg3OTg3NzMz.X4N1rA.aoO_TijV1zJuJcSj1n6AnwyE3JA");
+    public static void main(String[] args) throws IOException {
+        String s = new File("").getAbsolutePath();
+        System.out.println(s);
+        String token = new Scanner(new File("\\src\\bot_tokens\\DiscordBot\\token.txt")).nextLine();
+        JDABuilder jdaBuilder = JDABuilder.createDefault(token);
         JDA jda;
 
         try {
