984ceb753108c4ebbfe9b96068ecc70cec2a2263
TheSaltChip <seb2500@gmail.com>
2020-10-12 15:03:10 +0200

    Changed directories for better overview

---
+++ DiscordBots/FirstBot/src/main/java/Bot.java
@@ -0,0 +1,28 @@
+import commands.GetMessageCount;
+import commands.WriteMessages;
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+
+import javax.security.auth.login.LoginException;
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Bot {
+    public static void main(String[] args) throws IOException {
+        String token = new Scanner(new File("src\\BotTokens\\DiscordBot\\token.txt")).nextLine();
+        JDABuilder jdaBuilder = JDABuilder.createDefault(token);
+        JDA jda;
+
+        try {
+            jda = jdaBuilder.build();
+            jda.addEventListener(new GetMessageCount(jda));
+            jda.addEventListener(new WriteMessages(jda));
+
+        } catch (LoginException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+}
