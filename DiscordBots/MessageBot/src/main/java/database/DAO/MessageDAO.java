350c9aea9f437e90879553fd110dc1c79c7832cb
TheSaltChip <seb2500@gmail.com>
2020-10-15 01:40:07 +0200

    Made MessageDAO and added 3 methods, also JavaDoc-ed them

---
+++ DiscordBots/MessageBot/src/main/java/database/DAO/MessageDAO.java
@@ -0,0 +1,62 @@
+package database.DAO;
+
+import database.objects.MessageDB;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+
+@Stateless
+public class MessageDAO {
+    @PersistenceContext(name = "DiscordBotPU")
+    private EntityManager em;
+
+    /**
+     * Returns all the message-objects in the schema
+     *
+     * @return An unordered list of message objects
+     */
+    public List<MessageDB> getAllMessages() {
+        return em.createQuery("select m from MessageDB m", MessageDB.class).getResultList();
+    }
+
+    /**
+     * Attempts to get all the messages sent by a given user
+     * Returns an empty list if the given user has not written a message
+     *
+     * @param username      The actual name of the user
+     * @param discriminator The 4 digit discord-tag starting with a #, example: #1234
+     * @return List of all the messages sent by the given user
+     */
+    public List<MessageDB> getMessagesFromOneUser(String username, String discriminator) {
+        return em.createQuery("select m from MessageDB m where m.author = :username", MessageDB.class)
+                .setParameter("username", username + discriminator)
+                .getResultList();
+    }
+
+    /**
+     * Attemps to find a message with the given id
+     *
+     * @param id The id of the message you want
+     * @return The message with the given id if it exists
+     */
+    public MessageDB getMessageById(int id) {
+        return em.find(MessageDB.class, id);
+    }
+
+
+    /**
+     * Supposedly this method makes it possible to search for messages
+     * containing a substring
+     *
+     * @param searchTerm The string you want to search for
+     * @return A list of all the messages that contains the searchTerm
+     */
+    public List<MessageDB> searchAfterMessageWithString(String searchTerm) {
+        return em.createQuery("select m from MessageDB m where locate(m.msg_content, :search) > 0 ", MessageDB.class)
+                .setParameter("search", searchTerm)
+                .getResultList();
+    }
+
+}
