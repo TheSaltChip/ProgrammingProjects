6be2d8c5f3f6fcb8f2ba6a20cbb56184092ef9db
TheSaltChip <seb2500@gmail.com>
2020-10-16 13:11:26 +0200

    The sql file for setting up the database

---
+++ DiscordBots/MessageBot/src/sql/setup.sql
@@ -0,0 +1,23 @@
+DROP TABLE "guild".member cascade;
+DROP TABLE "guild".message cascade;
+
+set search_path to "guild";
+
+CREATE TABLE member
+(
+    user_id varchar(18) not null,
+    username varchar(32)  NOT NULL check (length(username) >= 2),
+    discriminator varchar(5) NOT NULL check (length(discriminator) = 5),
+    nickname varchar(32) check (length(nickname) >= 1),
+    PRIMARY KEY (user_id)
+);
+
+CREATE TABLE message
+(
+    message_id varchar(18) NOT NULL ,
+    author_id varchar(18),
+    author varchar(37) check (length(author) >= 7),
+    msg_content varchar(2000),
+    PRIMARY KEY (message_id),
+    FOREIGN KEY (author_id) references member(user_id)
+);
