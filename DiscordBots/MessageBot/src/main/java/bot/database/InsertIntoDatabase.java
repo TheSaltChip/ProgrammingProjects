6085f0d125eb6a63b3f36bb33d8bb28c8283eafb
TheSaltChip <seb2500@gmail.com>
2020-10-16 13:01:46 +0200

    Changed a logic error in getAllUsersAndInsert() and made getAllMessagesAndInsert() gather all the messages in the discord channel

--- DiscordBots/MessageBot/src/main/java/bot/database/InsertIntoDatabase.java
+++ DiscordBots/MessageBot/src/main/java/bot/database/InsertIntoDatabase.java
@@ -6,17 +6,14 @@ import database.objects.MemberDB;
 import database.objects.MessageDB;
 import net.dv8tion.jda.api.entities.*;
 
-import javax.ejb.EJB;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class InsertIntoDatabase {
     private final TextChannel TEXT_CHANNEL;
-    
-    @EJB
+
     MessageDAO messageDAO = new MessageDAO();
 
-    @EJB
     MemberDAO memberDAO = new MemberDAO();
 
     public InsertIntoDatabase(TextChannel textChannel) {
@@ -33,7 +30,7 @@ public class InsertIntoDatabase {
 
         List<MemberDB> memberDBS = members.stream()
                 .map(this::convertToMemberDB)
-                .filter(m -> memberDAO.checkIfUserExistsId(m.getUser_id()))
+                .filter(m -> !memberDAO.checkIfUserExistsId(m.getUser_id()))
                 .collect(Collectors.toList());
 
         memberDAO.insertMembers(memberDBS);
@@ -41,12 +38,11 @@ public class InsertIntoDatabase {
 
     private void getAllMessagesAndInsert() {
         MessageHistory messageHistory = TEXT_CHANNEL.getHistory();
-        List<Message> messages;
+        List<Message> messages = null;
 
-        while ((messages = messageHistory.getRetrievedHistory()).size() > 0) {
-            messageHistory.retrievePast(100).queue();
+        while (messages == null || !messages.isEmpty()) {
+            messages = messageHistory.retrievePast(100).complete();
             List<Message> newMessages = getMessagesFromMessageHistory(messages);
-
             List<MessageDB> dbList = newMessages.stream()
                     .map(this::convertToMessageDB)
                     .collect(Collectors.toList());
@@ -61,7 +57,7 @@ public class InsertIntoDatabase {
         MemberDB memberDB = new MemberDB();
         memberDB.setUser_id(user.getId());
         memberDB.setUsername(user.getName());
-        memberDB.setDiscriminator(user.getDiscriminator());
+        memberDB.setDiscriminator("#" + user.getDiscriminator());
         memberDB.setNickname(member.getNickname());
 
         return memberDB;
@@ -76,6 +72,7 @@ public class InsertIntoDatabase {
     private MessageDB convertToMessageDB(Message message) {
         MessageDB messageDB = new MessageDB();
         messageDB.setMessage_id(message.getId());
+        System.out.println(message.getAuthor().getId());
         messageDB.setAuthor(memberDAO.findMemberById(message.getAuthor().getId()));
         messageDB.setMsg_content(message.getContentStripped());
 
