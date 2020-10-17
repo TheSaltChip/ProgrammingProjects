2c71c27341c71bc98dd1b40e7cba8c7bc164a8af
TheSaltChip <seb2500@gmail.com>
2020-10-18 00:09:41 +0200

    Removed unused imports

--- DiscordBots/MessageBot/src/main/java/bot/commands/SetupDatabase.java
+++ DiscordBots/MessageBot/src/main/java/bot/commands/SetupDatabase.java
@@ -2,15 +2,12 @@ package bot.commands;
 
 import bot.database.InsertIntoDatabase;
 import net.dv8tion.jda.api.JDA;
-import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.entities.TextChannel;
 import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import org.jetbrains.annotations.NotNull;
-
-import java.util.List;
 
 public class SetupDatabase extends ListenerAdapter {
     JDA jda;
@@ -31,7 +28,7 @@ public class SetupDatabase extends ListenerAdapter {
             channel.sendMessage("Initiating data gathering").queue();
             intoDatabase.run();
             channel.sendMessage("Data gathering complete").queue();
-        } else if(msg.equals("!setup") && !user.getName().equals("TheSaltChip") && user.getDiscriminator().equals("8756")){
+        } else if (msg.equals("!setup") && !user.getName().equals("TheSaltChip") && user.getDiscriminator().equals("8756")) {
             channel.sendMessage("Only \"TheSaltChip#8756\" is allowed to use this command").queue();
         }
     }
