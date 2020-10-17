dc9c5070a6d5560ac52816592b45d96655725630
TheSaltChip <seb2500@gmail.com>
2020-10-18 01:50:12 +0200

    Added a case for if the user is mentioned

--- DiscordBots/MessageBot/src/main/java/bot/commands/Analyze.java
+++ DiscordBots/MessageBot/src/main/java/bot/commands/Analyze.java
@@ -1,7 +1,7 @@
 package bot.commands;
 
+import bot.database.AnalyzeDatabase;
 import net.dv8tion.jda.api.entities.Message;
-import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import org.jetbrains.annotations.NotNull;
@@ -12,6 +12,7 @@ public class Analyze extends ListenerAdapter {
         Message m = event.getMessage();
 
         if (m.getContentStripped().strip().equals("!analyze")) {
+
             if (m.getContentStripped().length() == 8) {
                 event.getChannel().sendMessage("This is the !analyze command\n" +
                         "To use this command write the username of the person you want to analyze\n" +
@@ -19,8 +20,14 @@ public class Analyze extends ListenerAdapter {
 
             } else if (m.getContentStripped().strip().length() > 9) {
                 if (m.getMentionedMembers().size() == 1) {
-                    User user = event.getMessage().getMentionedUsers().get(0);
+                    AnalyzeDatabase aD = new AnalyzeDatabase(event.getChannel(), event.getMessage().getMentionedUsers().get(0));
 
+                    if (!aD.exists()) {
+                        event.getChannel().sendMessage("User not found in the Database, run !setup to update the database").queue();
+
+                    } else {
+
+                    }
                 }
 
             }
