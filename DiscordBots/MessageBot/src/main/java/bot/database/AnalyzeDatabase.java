f94b8961c51f37f1eec66f5701e45b1582c5a3ee
TheSaltChip <seb2500@gmail.com>
2020-10-18 00:49:41 +0200

    Added AnalyzeDatabase.java which is going to be used gather data from the database and then process it

---
+++ DiscordBots/MessageBot/src/main/java/bot/database/AnalyzeDatabase.java
@@ -0,0 +1,24 @@
+package bot.database;
+
+import database.DAO.member.MemberBotDAO;
+import database.DAO.message.MessageBotDAO;
+import net.dv8tion.jda.api.entities.TextChannel;
+import net.dv8tion.jda.api.entities.User;
+
+public class AnalyzeDatabase {
+    private final TextChannel TEXT_CHANNEL;
+    private final User USER;
+    MessageBotDAO messageBotDAO = new MessageBotDAO();
+    MemberBotDAO memberBotDAO = new MemberBotDAO();
+
+    public AnalyzeDatabase(TextChannel text_channel, User user) {
+        TEXT_CHANNEL = text_channel;
+        USER = user;
+    }
+
+    public boolean exists(User user){
+        return memberBotDAO.exists(user.getId());
+    }
+
+
+}
