3a0756b593b58fb890afd6fcd213ff306cb4da2e
BuildTools <unconfigured@null.spigotmc.org>
2020-10-18 13:53:33 +0200

    Renamed MemberDB -> UserDB

--- DiscordBots/MessageBot/src/main/java/bot/database/AnalyzeDatabase.java
+++ DiscordBots/MessageBot/src/main/java/bot/database/AnalyzeDatabase.java
@@ -1,6 +1,6 @@
 package bot.database;
 
-import database.DAO.member.MemberBotDAO;
+import database.DAO.user.UserBotDAO;
 import database.DAO.message.MessageBotDAO;
 import net.dv8tion.jda.api.entities.TextChannel;
 import net.dv8tion.jda.api.entities.User;
@@ -9,7 +9,7 @@ public class AnalyzeDatabase {
     private final TextChannel TEXT_CHANNEL;
     private final User USER;
     MessageBotDAO messageBotDAO = new MessageBotDAO();
-    MemberBotDAO memberBotDAO = new MemberBotDAO();
+    UserBotDAO userBotDAO = new UserBotDAO();
 
     public AnalyzeDatabase(TextChannel text_channel, User user) {
         TEXT_CHANNEL = text_channel;
@@ -17,7 +17,7 @@ public class AnalyzeDatabase {
     }
 
     public boolean exists(){
-        return memberBotDAO.exists(USER.getId());
+        return userBotDAO.exists(USER.getId());
     }
 
 
