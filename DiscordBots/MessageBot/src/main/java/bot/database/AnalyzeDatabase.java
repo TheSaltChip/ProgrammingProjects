007f903f1a3a7e5fff74ef1bf0bd27b86c8459e2
TheSaltChip <seb2500@gmail.com>
2020-10-19 02:04:43 +0200

    Made all fields final and added more methods

--- DiscordBots/MessageBot/src/main/java/bot/database/AnalyzeDatabase.java
+++ DiscordBots/MessageBot/src/main/java/bot/database/AnalyzeDatabase.java
@@ -1,26 +1,59 @@
 package bot.database;
 
-import database.dao.user.UserBotDAO;
+import analyzing.Analyzer;
+import database.dao.info.InfoBotDAO;
 import database.dao.message.MessageBotDAO;
+import database.dao.user.UserBotDAO;
+import database.objects.MessageDB;
 import net.dv8tion.jda.api.entities.TextChannel;
 import net.dv8tion.jda.api.entities.User;
+
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class AnalyzeDatabase {
     private final TextChannel TEXT_CHANNEL;
     private final User USER;
-    MessageBotDAO messageBotDAO = new MessageBotDAO();
-    UserBotDAO userBotDAO = new UserBotDAO();
+    private final MessageBotDAO MESSAGE_BOT_DAO = new MessageBotDAO();
+    private final UserBotDAO USER_BOT_DAO = new UserBotDAO();
+    private final InfoBotDAO INFO_BOT_DAO = new InfoBotDAO();
+    private final Analyzer ANALYZER = new Analyzer();
 
     public AnalyzeDatabase(TextChannel text_channel, User user) {
         TEXT_CHANNEL = text_channel;
         USER = user;
     }
 
-    public boolean exists(){
-        return userBotDAO.exists(USER.getId());
+    public Map<Character, Integer> computeLetters() {
+        List<String> messages = USER_BOT_DAO.get(USER.getId()).getMessages().stream().map(MessageDB::getMsg_content).collect(Collectors.toList());
+        Map<Character, Integer> letters = new TreeMap<>(
+                Comparator.comparingInt(c -> c)
+        );
+
+        messages.forEach(m -> m.chars()
+                .mapToObj(i -> (char) i)
+                .forEach(c -> {
+                    if (!letters.containsKey(c)) {
+                        letters.put(c, 1);
+                    } else {
+                        letters.compute(c, (k, i) -> (i == null) ? 1 : i + 1);
+                    }
+                })
+        );
+
+        return letters;
+
     }
 
-    public void getWords(){
+    public boolean exists() {
+        return USER_BOT_DAO.exists(USER.getId());
+    }
 
+    public Map<Character, Integer> getLetters() {
+        return INFO_BOT_DAO.getLetters(USER.getId());
+    }
+
+    public void insert(Map<Character, Integer> lettersInfo) {
+        INFO_BOT_DAO.insert(lettersInfo);
     }
 }
