d962e66f833243fc2ca0721ca0aa97fa05da2607
TheSaltChip <seb2500@gmail.com>
2020-10-19 22:57:25 +0200

    Changed som logic and added a scenario for a coming !update command

--- DiscordBots/MessageBot/src/main/java/bot/commands/Setup.java
+++ DiscordBots/MessageBot/src/main/java/bot/commands/Setup.java
@@ -1,6 +1,6 @@
 package bot.commands;
 
-import bot.database.InsertIntoDatabase;
+import bot.database.DatabaseUtil;
 import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.entities.TextChannel;
 import net.dv8tion.jda.api.entities.User;
@@ -9,7 +9,7 @@ import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import org.jetbrains.annotations.NotNull;
 
 public class Setup extends ListenerAdapter {
-
+    private final DatabaseUtil SETUP = new DatabaseUtil();
     @Override
     public void onGuildMessageReceived(@NotNull GuildMessageReceivedEvent event) {
         Message message = event.getMessage();
@@ -17,13 +17,27 @@ public class Setup extends ListenerAdapter {
         TextChannel channel = event.getChannel();
         User user = message.getAuthor();
 
-        if (msg.equals("!setup") && user.getName().equals("TheSaltChip") && user.getDiscriminator().equals("8756")) {
-            InsertIntoDatabase intoDatabase = new InsertIntoDatabase(channel);
-            channel.sendMessage("Initiating data gathering").queue();
-            intoDatabase.run();
-            channel.sendMessage("Data gathering complete").queue();
-        } else if (msg.equals("!setup") && !user.getName().equals("TheSaltChip") && user.getDiscriminator().equals("8756")) {
-            channel.sendMessage("Only \"TheSaltChip#8756\" is allowed to use this command").queue();
+        if (msg.equals("!setup")) {
+            if (user.getName().equals("TheSaltChip") && user.getDiscriminator().equals("8756")) {
+                if (SETUP.hasRun()) {
+                    channel.sendMessage("The channel has already been setup\nUse !update to update the values").queue();
+                    return;
+                }
+
+                channel.sendMessage("Initiating data gathering").queue();
+                SETUP.setup(channel);
+                channel.sendMessage("Data gathering complete").queue();
+            } else {
+                channel.sendMessage("Only \"TheSaltChip#8756\" is allowed to use this command").queue();
+            }
+        } else if (msg.equals("!update")) {
+            if(user.getName().equals("TheSaltChip") && user.getDiscriminator().equals("8756")){
+                channel.sendMessage("Initiating data gathering").queue();
+                SETUP.update(channel);
+                channel.sendMessage("Data gathering complete").queue();
+            } else {
+                channel.sendMessage("Only \"TheSaltChip#8756\" is allowed to use this command").queue();
+            }
         }
     }
 }
