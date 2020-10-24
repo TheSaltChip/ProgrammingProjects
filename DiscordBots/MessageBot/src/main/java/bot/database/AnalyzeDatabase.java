7d3c3b101e0bc495d1329ef87bdacc3b66a46b84
TheSaltChip <seb2500@gmail.com>
2020-10-24 14:39:13 +0200

    Changed from Map to List on letter and word

--- DiscordBots/MessageBot/src/main/java/bot/database/AnalyzeDatabase.java
+++ DiscordBots/MessageBot/src/main/java/bot/database/AnalyzeDatabase.java
@@ -7,7 +7,9 @@ import database.dao.info.InfoBotDAO;
 import database.dao.message.MessageBotDAO;
 import database.dao.user.UserBotDAO;
 import database.objects.Info;
+import database.objects.Letter;
 import database.objects.MessageDB;
+import database.objects.Word;
 import net.dv8tion.jda.api.entities.User;
 
 import java.util.*;
@@ -30,33 +32,29 @@ public class AnalyzeDatabase {
                 .collect(Collectors.toList());
 
         //Compute the info about the letters
-        Map<Character, Integer> letters = new TreeMap<>(
-                Comparator.comparingInt(c -> c)
-        );
+        List<Letter> letters = new LinkedList<>();
 
         messages.forEach(m -> m.chars()
                 .mapToObj(i -> (char) i)
                 .forEach(c -> {
-                    if (!letters.containsKey(c)) {
-                        letters.put(c, 1);
+                    if (!letters.contains(c)) {
+                        letters.add(new Letter(c, 1));
                     } else {
-                        letters.compute(c, (k, i) -> (i == null) ? 1 : i + 1);
+                        letters.stream().filter(l -> l.getLetter() == c).forEach(l -> l.setTimes(l.getTimes()+1));
                     }
                 })
         );
 
         //Compute the info about the words
-        Map<String, Integer> words = new TreeMap<>(
-                Comparator.comparing(c -> c)
-        );
+        List<Word> words = new LinkedList<>();
 
         messages.stream()
                 .map(s -> s.split(" "))
                 .forEach(sA -> Arrays.stream(sA)
                         .filter(s -> !(s.matches("[,. ?!]") || s.length() > 100))
                         .forEach(s -> {
-                                    if (!words.containsKey(s)) {
-                                        words.put(s, 1);
+                                    if (!words.contains(s)) {
+                                        words.add();
                                     } else {
                                         words.compute(s, (k, i) -> (i == null) ? 1 : i + 1);
                                     }
@@ -64,7 +62,18 @@ public class AnalyzeDatabase {
                         )
                 );
 
-        if (INFO_DAO.get(USER.getId()) != null) {
+        if (INFO_DAO.get().isEmpty()) {
+            Info info = new Info(USER_DAO.get(USER.getId()), words, letters);
+
+            System.out.println(info.getId() +" " + info.getUser() + " " + info.getLetters() + " " + info.getWords());
+            System.out.println();
+            INFO_DAO.insert(info);
+            System.out.println("HELLO");
+            System.out.println();
+            USER_DAO.add(info);
+            System.out.println("GoodBye");
+
+        } else if (INFO_DAO.get(USER.getId()) != null) {
             Info info = INFO_DAO.get(USER.getId());
             System.out.println(info);
 
