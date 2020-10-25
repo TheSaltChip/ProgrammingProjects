44d9a577664b1a062a9c6ce370ed6422c3cefc9d
BuildTools <unconfigured@null.spigotmc.org>
2020-10-25 22:25:23 +0100

    Still trying to insert word and letter

--- DiscordBots/MessageBot/src/main/java/bot/database/AnalyzeDatabase.java
+++ DiscordBots/MessageBot/src/main/java/bot/database/AnalyzeDatabase.java
@@ -3,7 +3,9 @@ package bot.database;
 import database.dao.adt.InfoDAO;
 import database.dao.adt.UserDAO;
 import database.dao.info.InfoBotDAO;
+import database.dao.letter.LetterBotDAO;
 import database.dao.user.UserBotDAO;
+import database.dao.word.WordBotDAO;
 import database.objects.Info;
 import database.objects.Letter;
 import database.objects.MessageDB;
@@ -19,12 +21,13 @@ public class AnalyzeDatabase {
     private final User USER;
     private final UserDAO USER_DAO = new UserBotDAO();
     private final InfoDAO INFO_DAO = new InfoBotDAO();
+    private final LetterBotDAO LETTER_DAO = new LetterBotDAO();
+    private final WordBotDAO WORD_DAO = new WordBotDAO();
 
     public AnalyzeDatabase(User user) {
         USER = user;
     }
 
-    // TODO -- Override the contains method in each list to conform with my need
     public void compute() {
         List<String> messages = USER_DAO.get(USER.getId()).getMessages()
                 .stream()
@@ -45,11 +48,11 @@ public class AnalyzeDatabase {
 
                     if (letter == null) {
                         letter = new Letter(c, 1);
+                        letters.add(letter);
                     } else {
                         letter.incTimes();
-                        letters.remove(letter);
                     }
-                    letters.add(letter);
+
                 })
         );
 
@@ -78,16 +81,30 @@ public class AnalyzeDatabase {
                         )
                 );
 
+        LETTER_DAO.insert(letters);
+        WORD_DAO.insert(words);
+
         if (info == null) {
             info = new Info(USER_DAO.get(USER.getId()), words, letters);
+
+            for (int i = 0, j = 0;;){
+                if(i < words.size()){
+                    words.get(i).setInfo(info);
+                    i++;
+                }
+                if(j < letters.size()){
+                    letters.get(j).setInfo(info);
+                    j++;
+                }
+
+                if(j == letters.size() && i == words.size()) break;
+            }
 
             INFO_DAO.insert(info);
             USER_DAO.add(info);
 
         } else {
-            INFO_DAO.insert(USER.getId(), letters, words);
-
-
+            INFO_DAO.insert(info, letters, words);
         }
     }
 
