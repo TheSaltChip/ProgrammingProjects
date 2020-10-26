b71849ef4894a9ad60145b444c38229adc4a5561
TheSaltChip <seb2500@gmail.com>
2020-10-26 18:28:20 +0100

    Trying to fix Compute in AnalyzeDatabase

--- DiscordBots/MessageBot/src/main/java/Main.java
+++ DiscordBots/MessageBot/src/main/java/Main.java
@@ -1,19 +1,96 @@
+import database.dao.adt.InfoDAO;
+import database.dao.amount.AmountBotDAO;
+import database.dao.info.InfoBotDAO;
+import database.dao.letter.LetterBotDAO;
+import database.dao.letter_amount.LetterAmountBotDAO;
 import database.dao.message.MessageBotDAO;
 import database.dao.user.UserBotDAO;
-import database.objects.MessageDB;
+import database.objects.*;
 
+import java.util.LinkedList;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class Main {
+    private static final InfoDAO INFO_DAO = new InfoBotDAO();
+    private static final LetterAmountBotDAO LETTER_AMOUNT_DAO = new LetterAmountBotDAO();
     private static MessageBotDAO dao = new MessageBotDAO();
     private static UserBotDAO userBotDAO = new UserBotDAO();
+    private static AmountBotDAO amountBotDAO = new AmountBotDAO();
+    private static LetterBotDAO letterDAO = new LetterBotDAO();
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         List<String> messages = userBotDAO.get("159995722429628416").getMessages()
                 .stream()
                 .map(MessageDB::getMsg_content)
                 .collect(Collectors.toList());
+
+        Info info = INFO_DAO.get("159995722429628416");
+
+        if (info == null) {
+            info = new Info(userBotDAO.get("159995722429628416"));
+            INFO_DAO.insert(info);
+        }
+        //LETTERS
+        List<LetterAmount> letterAmounts = info.getLetters() == null ? new LinkedList<>() : info.getLetters();
+
+        List<Character> chars = messages.stream()
+                .map(s -> s.replaceAll(" ", ""))
+                .reduce("", (a, b) -> a + b)
+                .chars()
+                .mapToObj(c -> (char) c)
+                .collect(Collectors.toList());
+
+        for (Character c :
+                chars) {
+
+            LetterAmount letterAmount = letterAmounts.stream()
+                    .filter(l -> l != null && l.getLetter() != null)
+                    .filter(l -> l.getLetter().getLetter() == c)
+                    .findFirst().orElse(null);
+
+            if (letterAmount == null) {
+                System.out.println("NULL");
+
+                Letter l = letterDAO.get(c);
+
+                if (l == null) {
+                    l = new Letter(c);
+                    letterDAO.insert(l);
+                }
+
+                Amount a = amountBotDAO.get(1);
+                System.out.println("First a:    " + a);
+                if (a == null) {
+                    a = new Amount(1);
+                    amountBotDAO.insert(a);
+                }
+
+                Thread.sleep(5000);
+
+                letterAmount = new LetterAmount(INFO_DAO.get(info.getId()));
+
+                LETTER_AMOUNT_DAO.insert(letterAmount);
+
+                letterAmount = LETTER_AMOUNT_DAO.get(info.getId());
+
+                LETTER_AMOUNT_DAO.update(letterAmount, a, l);
+
+                letterAmounts.add(letterAmount);
+
+            } else {
+                System.out.println("NOT NULL");
+
+                int newA = letterAmount.getAmount().getAmount() + 1;
+
+                Amount a = amountBotDAO.get(newA);
+                a = a == null ? new Amount(newA) : a;
+
+                LETTER_AMOUNT_DAO.update(letterAmount, a, letterAmount.getLetter());
+            }
+
+            INFO_DAO.update(info, letterAmounts, null);
+        }
 
         System.out.println("Messages:\n" + messages + "\n\n");
 
