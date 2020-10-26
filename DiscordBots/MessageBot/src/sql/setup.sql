dd1118754ae5a3cc3891c007ec4503fa22d15f8d
TheSaltChip <seb2500@gmail.com>
2020-10-26 01:52:39 +0100

    Refactored. Added amount, letter, word, word_amount and letter_amount. Fixed all relations between them

--- DiscordBots/MessageBot/src/sql/setup.sql
+++ DiscordBots/MessageBot/src/sql/setup.sql
@@ -1,68 +1,76 @@
 DROP table IF EXISTS guild.user CASCADE;
 drop table if exists guild.message cascade;
-drop table if exists guild.info cascade;
-drop table if exists guild.letter cascade;
+drop table if exists guild.letterAmount cascade;
 drop table if exists guild.word cascade;
+drop table if exists guild.info cascade;
+drop table if exists guild.letter_amount cascade;
+drop table if exists guild.word_amount cascade;
+drop table if exists guild.amount cascade;
 
 set search_path to guild;
 
+-- USER
 CREATE TABLE "user"
 (
-    id            varchar(18) not null check (length(id) = 18),
+    id            varchar(18) PRIMARY KEY check (length(id) = 18),
     username      varchar(32) NOT NULL check (length(username) >= 2),
-    discriminator varchar(5)  NOT NULL check (length(discriminator) = 5),
-    info_id       int,
-    PRIMARY KEY (id)
+    discriminator varchar(5)  NOT NULL check (length(discriminator) = 5)
 );
 
+--MESSAGE
 CREATE TABLE message
 (
-    id          varchar(18) NOT NULL,
-    author_id   varchar(18),
-    msg_content varchar(2000),
-    time_created timestamp,
-    PRIMARY KEY (id),
-    FOREIGN KEY (author_id) references "user" (id)
+    id           varchar(18) PRIMARY KEY,
+    author_id    varchar(18) references "user" (id),
+    msg_content  varchar(2000),
+    time_created timestamp
 );
 
+--INFO 1 info has many word_amount and many letter_amount
 create table info
 (
-    id         serial,
-    user_id    varchar(18),
-    letters_id int,
-    words_id   int,
-    primary key (id),
-    foreign key (user_id) references "user" (id)
-);
-
-create table letter
-(
-    id serial,
-    letter  char,
-    times   int,
-    info_id int,
-    primary key (id),
-    foreign key (info_id) references info (id)
-);
-
-create table word
-(
-    id serial,
-    word    varchar(50),
-    times   int,
-    info_id int,
-    primary key (id),
-    foreign key (info_id) references info (id)
+    user_id varchar(18) references "user" (id) primary key
 );
 
 alter table "user"
-    add
-        foreign key (info_id) references info (id);
+    add info_id varchar(18) references info (user_id);
+
+--LETTER
+create table letterAmount
+(
+    letterAmount char primary key
+);
+
+-- WORD
+create table word
+(
+    word varchar(100) primary key
+);
+
+--AMOUNT
+create table amount
+(
+    amount int primary key
+);
+
+--WORD_AMOUNT 1 info has many word_amount
+create table word_amount
+(
+    collection varchar(18) references info (user_id) primary key,
+    word       varchar(100) references word (word),
+    amount      int references amount (amount)
+);
 
 alter table info
-    add
-        foreign key (letters_id) references letter (id);
+    add words varchar(18) references word_amount (collection);
+
+--LETTER_AMOUNT 1 info has many letter_amount
+create table letter_amount
+(
+    collection varchar(18) references info (user_id) primary key,
+    letterAmount     char references letterAmount (letterAmount),
+    amount      int references amount (amount)
+);
 
 alter table info
-    add
-        foreign key (words_id) references word (id);
+    add letterAmount varchar(18) references letter_amount (collection);
