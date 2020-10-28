016ad36f1b73c2810c5823b1472a01ce91c6e3be
TheSaltChip <seb2500@gmail.com>
2020-10-28 01:46:57 +0100

    Added a get all method

--- DiscordBots/MessageBot/src/main/java/database/dao/amount/AmountBotDAO.java
+++ DiscordBots/MessageBot/src/main/java/database/dao/amount/AmountBotDAO.java
@@ -6,6 +6,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.util.List;
 
 public class AmountBotDAO {
     private final EntityManagerFactory EMF = Persistence.createEntityManagerFactory("DiscordBotLocalPU");
@@ -19,6 +20,27 @@ public class AmountBotDAO {
             tx.begin();
 
             amount = em.find(Amount.class, num);
+
+            tx.commit();
+        } catch (Exception e) {
+            if (tx.isActive()) tx.rollback();
+
+        } finally {
+            em.close();
+        }
+
+        return amount;
+    }
+
+    public List<Amount> get() {
+        EntityManager em = EMF.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+        List<Amount> amount = null;
+
+        try {
+            tx.begin();
+
+            amount = em.createQuery("select a from Amount a",Amount.class).getResultList();
 
             tx.commit();
         } catch (Exception e) {
